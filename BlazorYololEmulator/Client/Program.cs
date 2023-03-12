4c68ed64bea5bb9e8a121953f9d9a99b4ea50050
Martin Evans <martindevans@gmail.com>
2023-03-12 15:56:11 +0000

    Updated packages

--- BlazorYololEmulator/Client/Program.cs
+++ BlazorYololEmulator/Client/Program.cs
@@ -2,6 +2,8 @@ using BlazorYololEmulator.Client;
 using BlazorYololEmulator.Client.Core;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
